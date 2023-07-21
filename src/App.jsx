802f43303dc49817c0225e03d496dc0d5bceec2f
jrf00020 <jrf00020@red.ujaen.es>
2023-07-21 13:06:48 +0200

    Add rating filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react'
 import { Movie } from './components/Movie'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
 import './App.css'
 import loading from './assets/loading.gif'
 
 export function App() {
   const [movies, setMovies] = useState([])
+  const [ratingFilter, setFilter] = useState('')
 
   const getMovies = async () => {
     const response = await fetch('https://favorite-movies-api.onrender.com/api/movies')
@@ -13,8 +17,13 @@ export function App() {
   }
 
   useEffect(() => {
+    console.log('useEffectMovies')
     getMovies()
   }, [])
+
+  const options = [
+    'Clear','⭐️', '⭐️⭐️', '⭐️⭐️⭐️', '⭐️⭐️⭐️⭐️', '⭐️⭐️⭐️⭐️⭐️'
+  ];
 
   if(movies.length === 0){
     return (
@@ -32,9 +41,18 @@ export function App() {
         <article className='mv-list'>
           <header>
             <h1>Movie List</h1>
+            <div className='mv-list-filters'>
+              <Dropdown options={options} menuClassName="mv-list-filters-menu" onChange={(e) => setFilter(e.value)} placeholder="Rating filter" />
+            </div>
           </header>
           <section className='mvs'>
-            {movies.map((movie) => (
+            {movies.filter(movie => {
+              if(ratingFilter === 'Clear' || ratingFilter === ''){
+                return movie
+              } else if(movie.rating === ratingFilter){
+                return movie
+              }
+            }).map((movie) => (
               <Movie key={movie.id} {...movie} />
             ))}
           </section>      
